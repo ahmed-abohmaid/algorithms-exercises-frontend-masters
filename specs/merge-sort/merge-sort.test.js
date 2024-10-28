eb94062557ee6dbf7c1ee9c654505221fd9f28ec
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-10-28 12:35:53 +0000

    solve: merge sort algorithm

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,35 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  if (nums.length < 2) {
+    return nums;
+  }
+  const middle = Math.floor(nums.length / 2);
+  const leftResult = mergeSort(nums.slice(0, middle));
+  const rightResult = mergeSort(nums.slice(middle));
+  return merge(leftResult, rightResult);
 };
+
+function merge(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  return result.concat(i < j ? arr1.slice(i) : arr2.slice(j));
+}
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
