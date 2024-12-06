6ea80e2c790d22de3be07213aa6cb7d25dc6851d
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2024-12-06 16:31:56 +0000

    feat: virtualize the bst solution

--- src/Tree.js
+++ src/Tree.js
@@ -4,23 +4,50 @@ import { TreeViz } from "./tree-visualizer";
 import _ from "lodash";
 
 class Tree {
-  add(num) {
-    // needs this
+  constructor() {
+    this.root = null;
+  }
+  add(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value < current.value) {
+          // Go left
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // Go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
   }
   toObject() {
-    // returns a root node with
-    // {
-    //   value:  <number>
-    //   left:   <object>
-    //   right:  <object>
-    //   height: <number, optional>
-    // }
-    return { value: 1, left: null, right: null, height: 1 };
+    return this.root;
+  }
+}
+
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 export default function TreeComponent() {
-  const nums = _.shuffle(_.range(50));
+  const nums = _.shuffle(_.range(10));
   const tree = new Tree();
   nums.map((num) => tree.add(num));
   const objs = tree.toObject();
